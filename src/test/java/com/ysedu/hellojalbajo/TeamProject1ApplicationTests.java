bf427699b58eb1e877fc66ab10461dc68f277652
jelee <waftyann@gmail.com>
2024-10-02 14:18:07 +0900

    프로젝트 생성

---
+++ src/test/java/com/ysedu/hellojalbajo/TeamProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysedu.hellojalbajo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
